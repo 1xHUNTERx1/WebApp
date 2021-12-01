eec7e98728a8a95e28a80ee08f8b429c3f0b385a
1xGUNTERx1 <rado.hristov22@gmail.com>
2021-12-01 14:46:16 +0200

    WebApplication2

---
+++ WebApplication2/Controllers/RecipesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace WebApplication2.Controllers
+{
+    public class AddRecipeInputModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime dateCooked { get; set; }
+        public string[] Ingredients { get; set; }   
+    }
+    public class RecipesController: Controller
+    {
+        public IActionResult Add(AddRecipeInputModel inputModel) 
+        {
+            return this.Json(inputModel);
+
+        }
+
+        public IActionResult Edit()
+        {
+            return this.View();
+        }
+    }
+
+}
